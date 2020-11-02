77fd522ba217f5ff99025c1ce6de3b02d8d99848
Manoj Daswani <ManojDaswani11@gmail.com>
2020-11-02 14:59:27 +0530

    Test View Controller 2nd Vov 2020

---
+++ TestVC/SearchVC.swift
@@ -0,0 +1,17 @@
+//
+//  SearchVC.swift
+//  TestVC
+//
+//  Created by Manoj Daswani on 2/11/20.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBlue
+    }
+}
